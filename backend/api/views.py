b1d58442205e5d2a31b6fe3e352134e256a8ed2d
AvailableNow <curretly.out.of.service@gmail.com>
2023-07-13 13:44:29 +0000

    third

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from .models import Task
 from .serializers import TaskSerializer
 
+
 class TaskView(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
@@ -12,3 +13,4 @@ class TaskView(viewsets.ModelViewSet):
         serializer = self.get_serializer(self.get_object())
         super().destroy(*args, **kwargs)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
