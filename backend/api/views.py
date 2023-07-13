1aface06bb20f37d6ecf7da2133607d428a5f726
AvailableNow <curretly.out.of.service@gmail.com>
2023-07-13 02:57:41 +0000

    ass

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.response import Response
-from rest_framework import status
 
 from .models import Task
-from .serializers import TaskSerializer
-
+from .serializers import TaskSerialir
 
 class TaskView(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
